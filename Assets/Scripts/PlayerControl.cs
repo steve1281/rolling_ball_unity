e9ec6a8db387c3856c8c3418934cd10264f62362
Steve Falcigno <steve1281@hotmail.com>
2016-02-05 20:29:25 -0500

    Files for a rollign ball tutorial.

---
+++ Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerControl : MonoBehaviour {
+	public float speed;
+
+	private Rigidbody rb;
+	void Start()
+	{
+		rb = GetComponent<Rigidbody>();
+	}
+
+	void Update()
+	{
+	}
+
+	void FixedUpdate()
+	{
+	// fixed is where physics go. We want to apply forces to our player.  This is physics. So this is where are code will go.
+      		float moveHorizontal = Input.GetAxis("Horizontal");
+      		float moveVertical = Input.GetAxis("Vertical");
+                Vector3 movement  = new Vector3 (moveHorizontal, 0 , moveVertical);
+		rb.AddForce(movement *speed);
+	}
+
+	void OnTriggerEnter(Collider other) 
+	{
+        	if (other.gameObject.CompareTag ("Pick Up"))  // you need to set the tag on the prefab to "Pick Up"
+        	{
+        		other.gameObject.SetActive (false);
+        	}
+    	}
+}
